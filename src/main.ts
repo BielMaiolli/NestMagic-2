3bfae4edb3cc28a259edeb1f97663270b0a0c632
leofranchini <142191956+leofranchini@users.noreply.github.com>
2024-09-20 23:18:08 -0300

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cluster = require('cluster');
+import { cpus } from 'os';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const isPrimary = cluster.isPrimary || cluster.isMaster;  
+
+  if (isPrimary) {
+    const numCPUs = cpus().length;
+    console.log(`Master process started. Forking ${numCPUs} workers...`);
+    
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork(); 
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died. Forking a new worker...`);
+      cluster.fork();
+    });
+  } else {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    console.log(`Worker ${process.pid} started`);
+  }
 }
+
 bootstrap();
